ff9c4767f0e844e93d1e351bfdcf5b56122decca
Elias Marcopoulos <emarcopoulos@gmail.com>
2016-06-02 11:20:37 -0400

    added index.js and procFile

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var ObjectId = require('mongodb').ObjectId;
+//var mongoUri = "mongodb://heroku_h11mcpnp:pgt40ln4i6rat9fac4f2v17bl@ds019829.mlab.com:19829/heroku_h11mcpnp"
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+// See https://stackoverflow.com/questions/5710358/how-to-get-post-query-in-express-node-js
+app.use(bodyParser.json());
+// See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP query or post parameters
+/*
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	db = databaseConnection;
+});
+*/
+/* CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  		response.send(__dirname + '/index.html');
+	});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
