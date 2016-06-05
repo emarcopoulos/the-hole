672b03c34494cabf669be5316038c77d0ae5f01f
Elias Marcopoulos <emarcopoulos@gmail.com>
2016-06-05 12:37:25 -0400

    added mongoDB for user storage

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var ObjectId = require('mongodb').ObjectId;
-//var mongoUri = "mongodb://heroku_h11mcpnp:pgt40ln4i6rat9fac4f2v17bl@ds019829.mlab.com:19829/heroku_h11mcpnp"
+var mongoUri = "mongodb://heroku_m4c5l1x1:n1q59l0vq10d5njkjc8hdgnig3@ds023"
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,11 +13,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 // See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
 app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP query or post parameters
-/*
+
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
-*/
 /* CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -29,8 +28,19 @@ app.use(function(req, res, next) {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
 	response.send(__dirname + '/index.html');
+});
+
+app.post('/saveGame', function (req, res) {
+	var toInsert = {_id:req.body.pStats.name};
+	toInsert[req.body.pStats.name] = req.body.pStats;
+	db.collection('users').update({_id:req.body.pStats.name}, toInsert, {upsert:true});
+	res.send(200);
+});
+
+app.get('/allStats', function (req, res) {
+	console.log(db.collection('users').find());
 });
 
 app.listen(app.get('port'), function() {
